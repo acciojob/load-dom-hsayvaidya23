28ec984e1f0958c1a400d93744ef4258598bf740
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 21:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.body.children.length === 0) {
+    document.body.textContent = "DOM load success";
+  }
+});
